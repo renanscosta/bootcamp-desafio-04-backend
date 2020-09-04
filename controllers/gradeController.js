9b67eb4e1f5ba31deabb08fe77b9b7dac4062681
renancosta.dsn.shop <renancosta.dsn.shop@alterdata.com.br>
2020-09-04 18:47:14 -0300

    commit inicial

---
+++ controllers/gradeController.js
@@ -0,0 +1,105 @@
+import { db } from '../models/index.js';
+import { logger } from '../config/logger.js';
+import { gradeModel } from '../models/gradeModel.js';
+
+const create = async (req, res) => {
+  try {
+    const grade = new gradeModel(req.body);
+    await grade.save();
+    res.send(grade);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Algum erro ocorreu ao salvar' });
+    logger.error(`POST /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findAll = async (req, res) => {
+  const name = req.query.name;
+
+  //condicao para o filtro no findAll
+  var condition = name
+    ? { name: { $regex: name, $options: 'i' } }
+    : {};
+
+  try {
+
+    const grade = await gradeModel.find(condition);
+    res.send(grade);
+
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Erro ao listar todos os documentos' });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const grade = await gradeModel.findById({ _id: id });
+    if (!grade) {
+      res.status(404).send({ message: 'Documento não encontrado' });
+    } else {
+
+      res.send(grade);
+    }
+
+
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const update = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Dados para atualizacao vazio',
+    });
+  }
+
+  const id = req.params.id;
+
+  try {
+    const grade = await gradeModel.findByIdAndUpdate({ _id: id }, req.body, { new: true });
+    res.send(grade);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
+    logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const grade = await gradeModel.findByIdAndDelete({ _id: id });
+
+    if (!grade) {
+      res.status(404).send({ message: 'Documento não encontrado na coleção' });
+    } else {
+      res.status(200).send();
+    }
+
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: 'Nao foi possivel deletar o Grade id: ' + id });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const removeAll = async (req, res) => {
+  try {
+    logger.info(`DELETE /grade`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+export default { create, findAll, findOne, update, remove, removeAll };
---
+++ models/gradeModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const gradeSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    subject: {
+        type: String,
+        require: true
+    },
+    type: {
+        type: String,
+        require: true
+    },
+    value: {
+        type: Number,
+        require: true
+    },
+    lastModified: {
+        type: Date,
+        default: Date.now
+    }
+});
+const gradeModel = mongoose.model('grades', gradeSchema, 'grades');
+export { gradeModel }
